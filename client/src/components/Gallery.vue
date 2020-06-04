f0ff1881a3cd44d190bfad9162c4b4917c506d5e
Pongsakorn <pcherng01@gmail.com>
2020-06-04 11:31:51 -0700

    Modified  the open-source material design template.

--- client/src/components/Gallery.vue
+++ client/src/components/Gallery.vue
@@ -3,23 +3,11 @@
     <mdb-row>
       <!-- Grid column -->
       <mdb-col lg="4" md="12" class="mb-4">
-        <a @click="showModal = true"><img class="img-fluid z-depth-1" src="https://mdbootstrap.com/img/screens/yt/screen-video-1.jpg" alt="video" data-toggle="modal" data-target="#modal1"></a>
+        <a @click="showModal = true"><img class="img-fluid z-depth-1" src="../../../resources/images/PIA04216_orig.jpg" alt="video" data-toggle="modal" data-target="#modal1"></a>
         <mdb-modal size="lg" :show="showModal" @close="showModal = false">
-          <mdb-modal-body class="mb-0 p-0">
-            <div class="embed-responsive embed-responsive-16by9 z-depth-1-half">
-              <iframe class="embed-responsive-item" src="https://www.youtube.com/embed/A3PDXmYoF5U"
-                allowfullscreen></iframe>
-            </div>
-          </mdb-modal-body>
+
           <mdb-modal-footer class="justify-content-center">
             <span class="mr-4">Spread the word!</span>
-            <a class="btn-floating btn-sm btn-fb"><i class="fab fa-facebook"></i></a>
-            <!--Twitter-->
-            <a class="btn-floating btn-sm btn-tw"><i class="fab fa-twitter"></i></a>
-            <!--Google +-->
-            <a class="btn-floating btn-sm btn-gplus"><i class="fab fa-google-plus"></i></a>
-            <!--Linkedin-->
-            <a class="btn-floating btn-sm btn-ins"><i class="fab fa-linkedin-in"></i></a>
             <mdb-btn outline="primary" rounded size="md" class="ml-4" @click.native="showModal = false">Close</mdb-btn>
           </mdb-modal-footer>
         </mdb-modal>
@@ -29,21 +17,9 @@
       <mdb-col lg="4" md="6" class="mb-4">
         <a @click="showModal2 = true"><img class="img-fluid z-depth-1" src="https://mdbootstrap.com/img/screens/yt/screen-video-2.jpg" alt="video" data-toggle="modal" data-target="#modal1"></a>
         <mdb-modal size="lg" :show="showModal2" @close="showModal2 = false">
-          <mdb-modal-body class="mb-0 p-0">
-            <div class="embed-responsive embed-responsive-16by9 z-depth-1-half">
-              <iframe class="embed-responsive-item" src="https://www.youtube.com/embed/wTcNtgA6gHs"
-                allowfullscreen></iframe>
-            </div>
-          </mdb-modal-body>
+
           <mdb-modal-footer class="justify-content-center">
             <span class="mr-4">Spread the word!</span>
-            <a class="btn-floating btn-sm btn-fb"><i class="fab fa-facebook"></i></a>
-            <!--Twitter-->
-            <a class="btn-floating btn-sm btn-tw"><i class="fab fa-twitter"></i></a>
-            <!--Google +-->
-            <a class="btn-floating btn-sm btn-gplus"><i class="fab fa-google-plus"></i></a>
-            <!--Linkedin-->
-            <a class="btn-floating btn-sm btn-ins"><i class="fab fa-linkedin-in"></i></a>
             <mdb-btn outline="primary" rounded size="md" class="ml-4" @click.native="showModal2 = false">Close</mdb-btn>
           </mdb-modal-footer>
         </mdb-modal>
@@ -53,21 +29,8 @@
       <mdb-col lg="4" md="6" class="mb-4">
         <a @click="showModal3 = true"><img class="img-fluid z-depth-1" src="https://mdbootstrap.com/img/screens/yt/screen-video-3.jpg" alt="video" data-toggle="modal" data-target="#modal1"></a>
         <mdb-modal size="lg" :show="showModal3" @close="showModal3 = false">
-          <mdb-modal-body class="mb-0 p-0">
-            <div class="embed-responsive embed-responsive-16by9 z-depth-1-half">
-              <iframe class="embed-responsive-item" src="https://www.youtube.com/embed/vlDzYIIOYmM"
-                allowfullscreen></iframe>
-            </div>
-          </mdb-modal-body>
           <mdb-modal-footer class="justify-content-center">
             <span class="mr-4">Spread the word!</span>
-            <a class="btn-floating btn-sm btn-fb"><i class="fab fa-facebook"></i></a>
-            <!--Twitter-->
-            <a class="btn-floating btn-sm btn-tw"><i class="fab fa-twitter"></i></a>
-            <!--Google +-->
-            <a class="btn-floating btn-sm btn-gplus"><i class="fab fa-google-plus"></i></a>
-            <!--Linkedin-->
-            <a class="btn-floating btn-sm btn-ins"><i class="fab fa-linkedin-in"></i></a>
             <mdb-btn outline="primary" rounded size="md" class="ml-4" @click.native="showModal3 = false">Close</mdb-btn>
           </mdb-modal-footer>
         </mdb-modal>
@@ -77,21 +40,8 @@
       <mdb-col lg="4" md="6" class="mb-4">
         <a @click="showModal4 = true"><img class="img-fluid z-depth-1" src="https://mdbootstrap.com/img/screens/yt/screen-video-4.jpg" alt="video" data-toggle="modal" data-target="#modal1"></a>
         <mdb-modal size="lg" :show="showModal4" @close="showModal4 = false">
-          <mdb-modal-body class="mb-0 p-0">
-            <div class="embed-responsive embed-responsive-16by9 z-depth-1-half">
-              <iframe class="embed-responsive-item" src="https://www.youtube.com/embed/vlDzYIIOYmM"
-                allowfullscreen></iframe>
-            </div>
-          </mdb-modal-body>
           <mdb-modal-footer class="justify-content-center">
             <span class="mr-4">Spread the word!</span>
-            <a class="btn-floating btn-sm btn-fb"><i class="fab fa-facebook"></i></a>
-            <!--Twitter-->
-            <a class="btn-floating btn-sm btn-tw"><i class="fab fa-twitter"></i></a>
-            <!--Google +-->
-            <a class="btn-floating btn-sm btn-gplus"><i class="fab fa-google-plus"></i></a>
-            <!--Linkedin-->
-            <a class="btn-floating btn-sm btn-ins"><i class="fab fa-linkedin-in"></i></a>
             <mdb-btn outline="primary" rounded size="md" class="ml-4" @click.native="showModal4 = false">Close</mdb-btn>
           </mdb-modal-footer>
         </mdb-modal>
@@ -101,21 +51,8 @@
       <mdb-col lg="4" md="6" class="mb-4">
         <a @click="showModal4 = true"><img class="img-fluid z-depth-1" src="https://mdbootstrap.com/img/screens/yt/screen-video-5.jpg" alt="video" data-toggle="modal" data-target="#modal1"></a>
         <mdb-modal size="lg" :show="showModal4" @close="showModal4 = false">
-          <mdb-modal-body class="mb-0 p-0">
-            <div class="embed-responsive embed-responsive-16by9 z-depth-1-half">
-              <iframe class="embed-responsive-item" src="https://www.youtube.com/embed/vlDzYIIOYmM"
-                allowfullscreen></iframe>
-            </div>
-          </mdb-modal-body>
           <mdb-modal-footer class="justify-content-center">
             <span class="mr-4">Spread the word!</span>
-            <a class="btn-floating btn-sm btn-fb"><i class="fab fa-facebook"></i></a>
-            <!--Twitter-->
-            <a class="btn-floating btn-sm btn-tw"><i class="fab fa-twitter"></i></a>
-            <!--Google +-->
-            <a class="btn-floating btn-sm btn-gplus"><i class="fab fa-google-plus"></i></a>
-            <!--Linkedin-->
-            <a class="btn-floating btn-sm btn-ins"><i class="fab fa-linkedin-in"></i></a>
             <mdb-btn outline="primary" rounded size="md" class="ml-4" @click.native="showModal4 = false">Close</mdb-btn>
           </mdb-modal-footer>
         </mdb-modal>
