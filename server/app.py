3a17c3d694e31146e2cb7575daaa4bc9fdfcf67a
Pongsakorn <pcherng01@gmail.com>
2020-06-04 11:24:35 -0700

    Modified the server file.

--- server/app.py
+++ server/app.py
@@ -1,5 +1,7 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, send_file
 from flask_cors import CORS
+import json
+import os, fnmatch
 
 
 # configuration
@@ -17,6 +19,24 @@ CORS(app, resources={r'/*': {'origins': '*'}})
 @app.route('/ping', methods=['GET'])
 def ping_pong():
     return jsonify('pong!')
+
+@app.route('/images/metadata/<name>', methods=['GET'])
+def getMetaData(name):
+    metadataName = '../resources/metadata/%s.json' % name
+    with open(metadataName) as f:
+        data = json.load(f)
+    return data
+
+@app.route('/images/<name>', methods=['GET'])
+def getImage(name):
+    imageName = '../resources/images/%s.jpg' % name
+    return send_file(imageName, mimetype='image/gif')
+
+@app.route('/images/list', methods=['GET'])
+def getImageList():
+    arr = fnmatch.filter(os.listdir('../resources/images'), '*.jpg')
+    arr = [os.path.splitext(file)[0] for file in arr]
+    return json.dumps(arr);
 
 
 if __name__ == '__main__':
